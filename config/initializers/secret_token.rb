173d834a84bde7eafad8e1b3be3efb7a3db2eab1
Andy Steinmann <andyast@gmail.com>
2013-04-29 07:53:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouchRepli::Application.config.secret_token = '91e32ed35b4da7d07683609aaed7853c3aa3f521a4708e72c9f00c126351569a958c09bbe15d0f474f6dc03ea3a1723cdd79033a1c73a47912d60e0ff8fa3bbb'
